90c5cad20310e0b6d8e4a0eb0aa846b4e0f7e1e1
Fei Xin Duke <113947184+FXDuke@users.noreply.github.com>
2022-10-19 09:00:30 -0400

    Lua Tables

--- Table.js
+++ Table.js
@@ -15,23 +15,23 @@ class Table {
     __Data = []
     Size = 0
     pairs = function(newFunction) {
-      var __Index = 0
-      for (var data of this.__Data) {
+      let __Index = 0
+      for (const data of this.__Data) {
         newFunction(__Index,data)
         __Index++
       }
     }
     add = function(...data) {
       if (data!=null) {
-        for (var i = 0;i<=data.length-1;i++) {
-          this.__Data.push(data[i])
+        for (const _item of data) {
+          this.__Data.push(_item)
         }
       }
       this.Size = this.__Data.length-1
     }
     remove = function(index) {
       this.__Data[index] = null
-      for (var newIndex = index+1;newIndex<this.__Data.length;newIndex++) {
+      for (let newIndex = index+1;newIndex<this.__Data.length;newIndex++) {
         this.__Data[newIndex-1] = this.__Data[newIndex]
       }
       this.__Data.length -= 1
@@ -39,8 +39,8 @@ class Table {
     }
     constructor(...data) {
       if (data!=null) {
-        for (var i = 0;i<=data.length-1;i++) {
-          this.__Data.push(data[i])
+        for (const _item of data) {
+          this.__Data.push(_item)
         }
         this.Size = this.__Data.length-1
       }
